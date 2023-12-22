68606fa99df78857d1366efbed78beb456688e43
Rachel E <117534780+rachel-easter@users.noreply.github.com>
2023-12-22 17:13:41 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,11 @@ class WordAnalyzer {
     this.resultDiv = document.getElementById('result') as HTMLDivElement;
   }
 
-  private isTextFile(file: File): boolean {
-    return file.name.toLowerCase().endsWith('.txt');
-  }
-
   private readDocument(callback: (content: string) => void): void {
     const file = this.documentInput.files?.[0];
 
     if (!file) {
-      alert('Please upload a file.');
-      return;
-    }
-
-    if (!this.isTextFile(file)) {
-      alert('Please upload a Text document with a .txt extension.');
+      alert('Please upload a Text document.');
       return;
     }
 
@@ -32,48 +23,39 @@ class WordAnalyzer {
       }
     };
 
-    reader.onerror = (event) => {
-      alert('Error reading the file.');
-      console.error(event);
-    };
-
     reader.readAsText(file);
   }
 
-  private analyzeLetters(content: string): void {
-    const letterCounts: Record<string, number> = {};
+  analyzeLetters = (): void => {
+    this.readDocument((documentContent) => {
+      const letterCounts: Record<string, number> = {};
 
-    for (const char of content) {
-      if (/[a-zA-Z]/.test(char)) {
-        const upperCaseChar = char.toUpperCase();
-        letterCounts[upperCaseChar] = (letterCounts[upperCaseChar] || 0) + 1;
+      for (const char of documentContent) {
+        if (/[a-zA-Z]/.test(char)) {
+          const upperCaseChar = char.toUpperCase();
+          letterCounts[upperCaseChar] = (letterCounts[upperCaseChar] || 0) + 1;
+        }
       }
-    }
 
-    const maxLetter = Object.keys(letterCounts).reduce((a, b) => letterCounts[a] > letterCounts[b] ? a : b);
+      const maxLetter = Object.keys(letterCounts).reduce((a, b) => letterCounts[a] > letterCounts[b] ? a : b);
 
-    const result = `Maximum Occurring Letter: ${maxLetter}\nLetter Counts: ${JSON.stringify(letterCounts)}`;
-    this.resultDiv.innerText = result;
-  }
-
-  private analyzePalindromes(content: string): void {
-    const modifiedContent = content.replace(/\b(\w+)\b/g, (match, word) => {
-      return word === word.split('').reverse().join('') ? '*'.repeat(word.length) : word;
+      const result = `Maximum Occurring Letter: ${maxLetter}\nLetter Counts: ${JSON.stringify(letterCounts)}`;
+      this.resultDiv.innerText = result;
     });
-
-    this.resultDiv.innerText = `Modified Content:\n${modifiedContent}`;
-  }
-
-  analyzeLettersButtonClick = (): void => {
-    this.readDocument(this.analyzeLetters);
   };
 
-  analyzePalindromeButtonClick = (): void => {
-    this.readDocument(this.analyzePalindromes);
+  analyzePalindromes = (): void => {
+    this.readDocument((documentContent) => {
+      const modifiedContent = documentContent.replace(/\b(\w+)\b/g, (match, word) => {
+        return word === word.split('').reverse().join('') ? '*'.repeat(word.length) : word;
+      });
+
+      this.resultDiv.innerText = `Modified Content:\n${modifiedContent}`;
+    });
   };
 }
 
 const wordAnalyzer = new WordAnalyzer();
 
-document.getElementById('analyzeLettersButton')?.addEventListener('click', wordAnalyzer.analyzeLettersButtonClick);
-document.getElementById('analyzePalindromeButton')?.addEventListener('click', wordAnalyzer.analyzePalindromeButtonClick);
+document.getElementById('analyzeLettersButton')?.addEventListener('click', wordAnalyzer.analyzeLetters);
+document.getElementById('analyzePalindromeButton')?.addEventListener('click', wordAnalyzer.analyzePalindromes);
